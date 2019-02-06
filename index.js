db3780880279412b79a11a175107c2d76b7b3bce
Joseph Briggs <jbriggsr3@gmail.com>
2019-02-06 12:51:48 +0000

    uncommented index.js

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ const asyncSync = require('./async_sync');
 
 // Spread operators & Rest operator
 
-// spreadNRest.info();
+spreadNRest.info();
 spreadNRest.spread();
-// spreadNRest.rest();
+spreadNRest.rest();
 
 
 // Destructuring
 
-// destructuring.info();
-// destructuring.arrays();
-// destructuring.objects();
+destructuring.info();
+destructuring.arrays();
+destructuring.objects();
 
 
 // Mutable and Immutable
 
-// mutableNimutable.info();
-// mutableNimutable.lesson1();
-// mutableNimutable.lesson2();
-// mutableNimutable.lesson3();
-// mutableNimutable.lesson3Result();
-// mutableNimutable.lesson4();
-// mutableNimutable.lesson5();
-// mutableNimutable.lesson6();
+mutableNimutable.info();
+mutableNimutable.lesson1();
+mutableNimutable.lesson2();
+mutableNimutable.lesson3();
+mutableNimutable.lesson3Result();
+mutableNimutable.lesson4();
+mutableNimutable.lesson5();
+mutableNimutable.lesson6();
 
 
 // Async & Sync
 
-// asyncSync.info();
-// asyncSync.callbackHell();
-// asyncSync.promises();
-// asyncSync.promiseLesson2();
-// asyncSync.promiseLesson3();
-// asyncSync.promisesV2();
-// asyncSync.promisesOnSteroids();
+asyncSync.info();
+asyncSync.callbackHell();
+asyncSync.promises();
+asyncSync.promiseLesson2();
+asyncSync.promiseLesson3();
+asyncSync.promisesV2();
+asyncSync.promisesOnSteroids();
